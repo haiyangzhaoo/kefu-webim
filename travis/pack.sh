72209bda0999d593d206e901dc26159e2b4927cc
lixk <lixk@ti-net.com.cn>
2023-05-11 16:21:43 +0800

    增加版本号

--- travis/pack.sh
+++ travis/pack.sh
@@ -1,4 +1,3 @@
 # travis
 CUR_COMMIT_MSG=`git log --pretty='%s' -1`
-BRANCH_NAME=$TRAVIS_BRANCH CUR_COMMIT_MSG=$CUR_COMMIT_MSG node ./travis/valid/valid_commit.js
 TAG_NAME=$CUR_COMMIT_MSG npm run build_all
