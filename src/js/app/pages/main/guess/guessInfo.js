25296929535f7fe357fd9293d2873351f5919e39
刘涛 <10666779@qq.com>
2023-10-18 09:35:43 +0800

    DEMAND-5386 敦煌茹来机器人实现，访客端输入联想功能 erros fixed

--- src/js/app/pages/main/guess/guessInfo.js
+++ src/js/app/pages/main/guess/guessInfo.js
@@ -41,6 +41,7 @@ function _addEvents(){
 		var curText = this.innerText;
 		channel.sendText(curText);
 		doms.textInput.value = "";
+    oldValue = "";
 		utils.trigger(doms.textInput, "change");
 		// 恢复默认样式
 		doms.guessArea.style.display = "none";
@@ -63,7 +64,7 @@ function _addEvents(){
 	// keyup在ios手机原生的输入法是不支持的，但是在ios端第三方的输入法搜狗输入法是支持的，为了兼容性，不能使用keyup事件
 	utils.on(doms.textInput, "input propertychange", function(){
 		// doms.guessArea.style.display = "block";
-		var value = this.value;
+		var value = this.value.length ? this.value.trim() : "";
 		newValue = value;
 
     clearTimeout(timer);
@@ -74,7 +75,7 @@ function _addEvents(){
 			if(profile.currentOfficialAccount.agentId &&  profile.currentOfficialAccount.agentType == 6){
 				timer = setTimeout(function(){
           apiHelper.getGuessList(value).then(function(res){
-            if(res && res.data && res.data.entities && res.data.entities.length){
+            if(res && res.data && res.data.entities && res.data.entities.length && newValue.length){ // 如果搜索结果出来的时候 用户清空了输入框 则不再展示结果
               doms.guessArea.style.display = "block";
               doms.guessTips.innerText = "请点击您想咨询的问题！";
               doms.loading.style.display = "none";
@@ -91,7 +92,7 @@ function _addEvents(){
               doms.chatWrapper.style.bottom = 140 + "px";
             }
           });
-        }, 200)
+        }, 300)
 			}
 			else{
 				doms.guessArea.style.display = "none";
@@ -100,8 +101,8 @@ function _addEvents(){
 		}
 		// 当值不变时 不触发搜索状态
 		else if(oldValue == newValue){
-			doms.guessArea.style.display = "none";
-			doms.chatWrapper.style.bottom = 140 + "px";
+			// doms.guessArea.style.display = "none";
+			// doms.chatWrapper.style.bottom = 140 + "px";
 		}
 		else if(!value){
 			// 文本框内容清空时 重置样式
